44dddd611fa760c40ddd49223b0de412cd99b182
Naice <unaice301@gmail.com>
2023-02-21 14:01:08 -0800

    whenever

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,16 +4,29 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int count, sz;
+char str[] = "_putchar\n";
+int i = 0;
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+while (str[i] != '\0')
 	{
-		_putchar(str[count]);
+	char c = str[i];
+
+	_putchar(c);
+	i++;
 	}
-	_putchar('\n');
-	return (0);
+return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
