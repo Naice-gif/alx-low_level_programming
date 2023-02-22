47d31e30c38a14fdf3eddf0287740adcb4c55b55
Naice <unaice301@gmail.com>
2023-02-21 16:36:29 -0800

    I dont listen

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,8 +7,9 @@
  */
 void print_times_table(int n)
 {
-    	for(int i = 0; i<n; i++){
-    		for(int j = 0; j<n; j++){
+	int i, j;
+    	for(i = 0; i<n; i++){
+    		for(j = 0; j<n; j++){
     			printf("%d, ", i*j);
     		}
     		printf("\n");
