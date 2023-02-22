a59ac328e3c16b56c1936936b73ab7e5c916460b
Naice <unaice301@gmail.com>
2023-02-21 16:44:45 -0800

    nee d money

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
  * print_times_table - Entry point
  * @n: input
  * Return: Always 0 (Success)
  */
+
 void print_times_table(int n)
 {
 	int i, j;
-    	for(i = 0; i <= n; i++){
-    		for(j = 0; j <= n; j++){
-    			printf("%d, ", i*j);
+    	for (i = 0; i <= n; i++)
+	{
+    		for (j = 0; j <= n; j++)
+		{
+    			printf("%d,\t", i*j);
     		}
     		printf("\n");
     	}
