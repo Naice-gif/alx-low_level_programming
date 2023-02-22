b844efb144e62e84d242a93b8f6ac7f762766df4
Naice <unaice301@gmail.com>
2023-02-21 16:51:50 -0800

    The last Dance

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,11 +10,15 @@
 void print_times_table(int n)
 {
 	int i, j;
-    	for (i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-    		for (j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
-    			printf("%d,  ", i*j);
+			if (j < n){
+				printf("%d, ", i*j);
+			}
+			else
+    				printf("%d", i*j);
     		}
     		printf("\n");
     	}
