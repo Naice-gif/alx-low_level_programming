339db3d32577cda14d5ae86f1a127d3a5f52a2fe
Naice-gif <unaice301@gmail.com>
2023-04-13 10:34:12 -0700

    Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,19 +1,39 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdlib.h>
 
-/*
- * Desc: Header file containing prototypes for all functions
- *       written in the 0x15-file_io project.
+/**
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t p, d, w;
+	char *buffer;
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+	if (filename == NULL)
+		return (0);
 
-int _putchar(char c);
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
 
-#endif /* MAIN_H */
+	p = open(filename, O_RDONLY);
+	d = read(p, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, d);
+
+	if (p == -1 || d == -1 || w == -1 || w != d)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	clos(p);
+
+	return (w);
+}
